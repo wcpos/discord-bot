62a481f42cd87be8110c7218e390655da8d04f4e
Paul Kilmurray <paul@kilbot.com>
2023-03-16 22:36:04 +0100

    update messages

--- config.py
+++ config.py
@@ -19,29 +19,31 @@ Hi {name}, welcome to the '{GUILD}' Discord Server."""
 # Help message
 HELP_MSG = f"""\
 This bot is used to verify Pro licenses for access to the '{GUILD}' Discord Server. 
+Type your license key into a private message and I will check the license server. 
+If the license is valid you will be promoted to a Pro User.
 """
 
 # Unknown message
 UNKNOWN_MESSAGE = """\
-I could not understand your message.
-For further help use the command "help" .
+Hi! I am the license verification bot. I can verify your license key for access to the '{GUILD}' Discord Server. 
+Please send me your license key in a private message. DO NOT type your license key in the public channel.
 """
 
 # Successfull setting role
 SUCCESS_SETTING_ROLE = """\
-You have been verified. You will be given Pro access."""
+You have been verified. You now have access to the Pro User chat."""
 
 ERROR_SETTING_ROLE = """\
-Could not set role, please try again or contanct an admin."""
+Could not set role, please try again or contact an admin."""
 
 ERROR_LICENSE_NOT_VALID = """\
 Your license key is not valid. Please try again or contact an admin."""
 
 ERROR_LICENSE_SERVER_DOWN = """\
-I could not contact the license server, please try again in a moment or contact support."""
+I could not contact the license server, please try again in a moment or contact an admin."""
 
 ERROR_LICENSE_SERVER_ERROR = """\
 There was an error."""
 
 VERIFING_LICENSE = """\
-Verifying license. One momnet please..."""
+Verifying license. One moment please..."""
